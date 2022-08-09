e3a138c4e9cfb02ae362e36079e4207abe25092a
Elif <60880193+Elif-web@users.noreply.github.com>
2022-08-09 16:56:36 +0300

    Add files via upload

---
+++ array.map/index.js
@@ -0,0 +1,48 @@
+/*
+Challenge 1:
+Given an array of numbers, return an array of each number, squared
+*/
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+
+const map1 = nums.map(x => x**2)
+
+console.log(map1)
+
+
+
+/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"]
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+
+const map2 = names.map(x => x[0].toUpperCase().concat(x.substr(1)))
+
+console.log(map2)
+
+
+
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+const map3 =  pokemon.map(x => x.substr(0,0).concat("<p>").concat(x.substr(0)).concat("</p>"))
+console.log(map3)
+
+
